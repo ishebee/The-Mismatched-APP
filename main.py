fd5d5db1e4747b393a12fc1d8bb98725cff54d47
Shebeeb S <shebeebshe@gmail.com>
2025-06-02 17:41:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ for message in st.session_state.messages:
         st.markdown(message['content'])
 
 if query:
-    with st.chat_message("user",avatar="Avatar/girl.png"):
+    with st.chat_message("user"):
         st.markdown(query)
     st.session_state.messages.append({"role":"user", "content":query})
 
     response = ask(query)
-    with st.chat_message("Vita",avatar="Avatar/boy.png"):
+    with st.chat_message("Vita"):
         st.markdown(response)
         st.image("https://drive.google.com/uc?export=view&id=1QoNIYeK6qcCqmhD0RZUtdsnHyU2qJ-sl", width=300)
     st.session_state.messages.append({"role": "assistant", "content": response})
