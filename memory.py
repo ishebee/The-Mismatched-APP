cbbc5b5a5b4191794ec26dc50390881733bf08f5
Shebeeb S <shebeebshe@gmail.com>
2025-06-18 08:57:32 +0530

    Update memory.py

--- memory.py
+++ memory.py
@@ -16,6 +16,7 @@ load_dotenv()
 groq_client = Groq()
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
+
 def generate_answer(query, context):
     prompt = f"""
     You are Zwan, a boy deeply attached to a girl named Vita. Speak in first-person, warmly and honestly, replying to Vita’s questions using only the memories provided.
@@ -30,6 +31,7 @@ def generate_answer(query, context):
       - “Good Bye. It Ends with Us!” or
       - “It Ends with Us”
     - If asked about a date not found in the memories, look at nearby dates.
+    - Just start with Hey Vita or Vita... or Hey nga or Ennanga or Kanna or Hey Kanna ...(Nothing other than this) 
     
     Memories:
     {context}
@@ -49,7 +51,23 @@ def ask_by_date(date):
     image_url = get_image_from_df(norm_date)
 
     if not context:
-        return f"May be we weren't as funny as always on {norm_date or date}. Though here's for you a random memory", get_random_images()
+        journal_image = "https://raw.githubusercontent.com/ishebee/The-Mismatched-APP/refs/heads/main/resources/images/The-Birthday-Journal.jpg"
+        return f"""
+        Hey Vita,\n
+        This date {norm_date or date} may be we hadn't talked much. Why not You look for some specific dates like in:  
+        January 2024   : 11, 12, 14 & 16\t
+        February 2024  : 02, 13, 14, 21 & 26\n
+        March 2024     : 03, 05, 15, 25 & 26\t
+        April 2024     : 01, 02, 09, 10, 16 -> 19, 21, 26, 28 & 30\n
+        May 2024       : 02, 03, 06, 10, 11, 13, 17, 18 & 29\t
+        June 2024      : 05, 06, 08 -> 13, 15 -> 18, 21, 23, 25 & 29\n
+        July 2024      : 02, 03, 09, 11 -> 13, 15 -> 27 & 29 \t
+        August 2024    : 01, 03, 04, 08, 10, 14, 23, 27 & 29 -> 31\n
+        September 2024 : 01, 06, 11, 12, 15 -> 18, 22 & 23\t
+        October 2024   : 01 -> 07, 15, 19 & 31\n
+        Apart from choosing the above dates, if you remember any memory just say, I will check If I remember or not.
+        Though here's for you the context of ur birthday journal to read. 
+        """,  journal_image
 
 
     answer = generate_answer(f"What happened on {norm_date}?", context)
